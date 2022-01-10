d07cce7a5a29dac879b43c2400a10803b6c36137
Kenneth Martin <558152+kennethlmartin@users.noreply.github.com>
2022-01-10 02:30:08 -0500

    build: update eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,3 @@
 module.exports = {
-  env: {
-    node: true,
-  },
-  extends: [
-    'archetype/rules/eslint/best-practices.js',
-    'archetype/rules/eslint/errors.js',
-    'archetype/rules/eslint/style.js',
-    'archetype/rules/eslint/variables.js',
-  ],
+  extends: ['archetype/configurations/node'],
 };
